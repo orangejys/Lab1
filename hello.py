01f363b3d449ded11efac7b563878fa7a3d719b5
Муханов Саша <muhanovad2007@gmail.com>
2025-09-20 23:01:20 +0300

    Добавил операцию произведения

--- hello.py
+++ hello.py
@@ -4,3 +4,6 @@ num1 = int(input("Введите первое целое число: "))
 num2 = int(input("Введите второе целое число: "))
 
 print(f"Вы ввели: {num1} и {num2}")
+
+proizvedenie = num1 * num2
+print(proizvedenie)
